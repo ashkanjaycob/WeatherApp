7070a6c4483745180e879ef55aefdc1ed071d4db
ashkanjaycob <ashkan.yaghobii@gmail.com>
2023-04-17 12:18:04 +0430

    Change Location

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -3,10 +3,11 @@
 
         <nav class="navbar  rounded-pill my-2">
             <div class="container f-flex justify-content-center">
-                <a class="navbar-brand">Weather forecast by Ashkan Yaghobi</a>
+               <h1> <a class="navbar-brand">Weather forecast by Ashkan Yaghobi</a></h1>
                 <form class="d-flex w-75" role="search">
-                    <input class="form-control me-2 rounded-pill " type="search" placeholder="Search Your City ..." aria-label="Search">
-                    <button class="btn btn-outline-success rounded-pill w-50" type="submit">Search</button>
+                    <input class="form-control me-2 p-3 rounded-pill" v-model.lazy="city" type="search"
+                        placeholder="Search Your City Weather ..." aria-label="Search">
+                    <!-- <button @click="SeTcity()" class="btn btn-outline-success rounded-pill w-50" type="submit">Search</button> -->
                 </form>
             </div>
         </nav>
@@ -23,7 +24,7 @@
 <script>
 import axios from 'axios'
 import WeatherShow from './weatherShow.vue'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 export default {
     name: 'HeaderComp',
     components: {
@@ -31,36 +32,41 @@ export default {
     },
     setup() {
 
+
         const isTrue = ref(true);
 
         const Api = ref('235785bc369ab18214c37221df285463'
         );
         const data = ref({});
 
-        // Make a request for a user with a given ID
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Tehran&appid=${Api.value}`)
-            .then(function (response) {
-                isTrue.value = false;
-                data.value = response.data;
-                console.log(data.value);
-                console.log(response.headers['x-response-time'  ])
-                console.log(response);
-                
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            });
+        const city = ref("tehran");
 
-           function success() {
 
-            const receiveDate = (new Date());
-            console.log(receiveDate);
-           }
 
-           
-            return { Api, data, isTrue , success }
+        function SetCity() {
+            // Make a request for a user with a given ID
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${Api.value}`)
+                .then(function (response) {
+                    isTrue.value = false;
+                    data.value = response.data;
+                    console.log(data.value);
+                    city.value="";
+
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
         }
+        SetCity();
+
+        
+        watch(city, () => {
+            SetCity();
+        });
+
+        return { Api, data, isTrue, city, SetCity }
     }
+}
 </script>
 <style></style>
